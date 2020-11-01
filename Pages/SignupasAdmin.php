861a9c65784d77f369252459f877ced4f6ad9ac1
Ramballabh agrawal <ramballabh.agrawal_cs18@gla.ac.in>
2020-11-01 15:22:40 +0530

    create page for signup as admin

---
+++ Pages/SignupasAdmin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css"/>
+        <link rel="stylesheet" href="../css/Signup.css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <title>SignUpAsAdmin</title>
+    </head>
+    <body>
+        <div id="contanier">
+            <div id="header">
+                <div id="logo"><img src="../images/1.png"></div>
+                <ul class="menu">
+                    <li><a href="../index.html"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;HOME</a></li>
+                <li><a href="Aboutus.html"><i class="fa fa-users" aria-hidden="true"></i>&nbsp;ABOUT US</a></li>
+                <li><a href="Contact-Us.html"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;CONTACT</a></li>
+                <li><a href="PreLoginPage.html"><i class="fa fa-sign-in" aria-hidden="true"></i>&nbsp;LOGIN</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="middle">
+            <form method="POST">
+                <h1><center>Sign Up</center></h1><br>
+                <hr><br>
+                <h2><center>CREATE AN ACCOUNT</center></h2><br>
+                <label for="fname">First Name:</label>
+                <input type="text" placeholder="Enter First Name" name="fname" required>
+                <label for="lname">Last Name:</label>
+                <input type="text" placeholder="Enter Last Name" name="lname" required>
+                <label for="uname">Username:</label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+                <label for="email">Email:</label>
+                <input type="text" placeholder="Enter Email" name="email" required>
+                <label for="psw">Password:</label>
+                <input type="text" placeholder="Enter Password" name="psw" required>
+                <label for="rpsw">Repeat Password:</label>
+                <input type="text" placeholder="Enter Password Again" name="rpsw" required>
+                <button type="submit" class="signup" name="submit-btn" >Create An Account</button><br><br>
+                <h4>Already have an account? <a href="../Pages/Login.html">Sign-in</a></h4>
+            </form>
+        </div>
+    </body>
+</html>
