8e61d99762b3f92f55c6cc629f76c2d4ec9e27cc
Ramballabh Agrawal <ramballabh.agrawal_cs18@gla.ac.in>
2020-11-19 10:51:49 +0530

    update exam.php to disable the refresh action during exam

--- Pages/exam.php
+++ Pages/exam.php
@@ -20,6 +20,22 @@
     ?>
     <!--alert message end-->
     <script language="javascript">
+
+        document.onkeydown = function() {    
+            switch (event.keyCode) { 
+                case 116 : //F5 button
+                    event.returnValue = false;
+                    event.keyCode = 0;
+                    return false; 
+                case 82 : //R button
+                    if (event.ctrlKey) { 
+                        event.returnValue = false; 
+                        event.keyCode = 0;  
+                        return false; 
+                    } 
+            }
+        }
+        
         var Timer;
         var TotalSeconds;
 
